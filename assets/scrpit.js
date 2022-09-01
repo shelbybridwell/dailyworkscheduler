39588eebb69711e240ff852d84f5beedabd3271c
Shelby Bridwell <shelbybridwell@Shelbys-MacBook-Pro.local>
2022-09-01 11:49:47 -0500

    time tracker function

--- assets/scrpit.js
+++ assets/scrpit.js
@@ -11,3 +11,31 @@ $(document).ready(function () {
   // Save text in local storage
   localStorage.setItem(time, text);
 })
+function timeTracker() {
+    //get current number of hours.
+    var timeNow = moment().hour();
+
+    // loop over time blocks
+    $(".time-block").each(function () {
+        var blockTime = parseInt($(this).attr("id").split("hour")[1]);
+
+        // To check the time and add the classes for background indicators
+        if (blockTime < timeNow) {
+            $(this).removeClass("future");
+            $(this).removeClass("present");
+            $(this).addClass("past");
+        }
+        else if (blockTime === timeNow) {
+            $(this).removeClass("past");
+            $(this).removeClass("future");
+            $(this).addClass("present");
+        }
+        else {
+            $(this).removeClass("present");
+            $(this).removeClass("past");
+            $(this).addClass("future");
+
+        }
+    })
+}
+
