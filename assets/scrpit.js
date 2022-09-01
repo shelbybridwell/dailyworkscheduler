fc3c103c77b474e303d3f20e345e1c2cece16fe3
Shelby Bridwell <shelbybridwell@Shelbys-MacBook-Pro.local>
2022-09-01 11:43:38 -0500

    today's date

---
+++ assets/scrpit.js
@@ -0,0 +1,13 @@
+// Display today's day and date
+var todayDate = moment().format('dddd, MMM Do YYYY');
+$("#currentDay").html(todayDate);
+
+$(document).ready(function () {
+    // saveBtn click listener 
+    $(".saveBtn").on("click", function () {
+        // Get nearby values of the description in JQuery
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+  // Save text in local storage
+  localStorage.setItem(time, text);
+})
